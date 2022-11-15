f61a510ec20ea279cb76c2638dad8929b0650f6b
maher <maherramadan2012@hotmail.com>
2022-11-15 01:14:44 -0600

    Fix errors in main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-document.querySelector(".menu-link").addEventListener('click', (e)=>{
-    // alert('Click on hamburger')
-    document.querySelector('.mobile-menu-container').setAttribute("style", "visibility:visible");
-    e.preventDefault();
-  })
-  
-   document.querySelector(".mobile-menu-close").addEventListener('click', (e)=>{
-    document.querySelector('.mobile-menu-container').setAttribute("style", "visibility: hidden");
-    e.preventDefault();
-  })
-  
-  document.querySelector(".mobile-menu-1").addEventListener('click', (e)=>{
-    document.querySelector('.mobile-menu-container').setAttribute("style", "visibility:hidden");
-  })
-  
-  document.querySelector(".mobile-menu-2").addEventListener('click', (e)=>{
-    document.querySelector('.mobile-menu-container').setAttribute("style", "visibility:hidden");
-  })
-  
-  document.querySelector(".mobile-menu-3").addEventListener('click', (e)=>{
-    document.querySelector('.mobile-menu-container').setAttribute("style", "visibility:hidden");
-  })
-  
+document.querySelector('.menu-link').addEventListener('click', (e) => {
+  // alert('Click on hamburger')
+  document.querySelector('.mobile-menu-container').setAttribute('style', 'visibility:visible');
+  e.preventDefault();
+});
+
+document.querySelector('.mobile-menu-close').addEventListener('click', (e) => {
+  document.querySelector('.mobile-menu-container').setAttribute('style', 'visibility: hidden');
+  e.preventDefault();
+});
+
+document.querySelector('.mobile-menu-1').addEventListener('click', () => {
+  document.querySelector('.mobile-menu-container').setAttribute('style', 'visibility:hidden');
+});
+
+document.querySelector('.mobile-menu-2').addEventListener('click', () => {
+  document.querySelector('.mobile-menu-container').setAttribute('style', 'visibility:hidden');
+});
+
+document.querySelector('.mobile-menu-3').addEventListener('click', () => {
+  document.querySelector('.mobile-menu-container').setAttribute('style', 'visibility:hidden');
+});
